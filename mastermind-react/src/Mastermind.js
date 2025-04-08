276a2262f3458215ad3c955260e9c1d666e3088c
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-08 14:38:18 +0300

    Update Mastermind.js

--- mastermind-react/src/Mastermind.js
+++ mastermind-react/src/Mastermind.js
@@ -1,6 +1,10 @@
 import {useEffect, useRef, useState} from "react";
 import Container from "./components/common/container";
 import Card from "./components/common/card";
+import InputText from "./components/common/input-text";
+import Button from "./components/common/button";
+import Badge from "./components/common/badge";
+
 // Deadline -> 14:20
 function Mastermind() {
     //region define states
@@ -16,20 +20,36 @@ function Mastermind() {
     // endregion
 
     const timerId = useRef(null);
-    useEffect(()=>{
-      timerId.current = setInterval(()=>{
-          setCounter(counter - 1);
-      })
-      return() => {
-          if (timerId) clearInterval(timerId.current);
-      }
-    }, []);
+    useEffect(() => {
+        timerId.current = setInterval(() => {
+            setCounter(prev => prev - 1);
+        }, 1_000)
+        console.log("Mastermind component is mounted");
+        return () => {
+            console.log("Mastermind component is unmounted");
+            if (timerId) clearInterval(timerId.current);
+        }
+    },[]);
 
+    const play = () => {
+        
+    }
     //return View
     return (
         <Container>
             <Card title={"Mastermind Game Console"}>
-
+                <InputText label={"Guess"}
+                           value={guess}
+                           handleChange={(e) => setGuess(Number(e.target.value))}
+                           id={"guess"}
+                           explain={"Enter your guess"}/>
+                <Button label={"Play"}
+                        click={play}
+                        color={"btn-success"}/>
+                <Badge value={counter}
+                       label={"Counter"}
+                       color={"bg-warning"}
+                       isVisible={true}/>
             </Card>
         </Container>
     );
