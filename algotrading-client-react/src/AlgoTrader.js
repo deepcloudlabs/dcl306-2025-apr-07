ed56a67433300d085a8468ff1725ed1e96161692
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-09 16:37:21 +0300

    Update AlgoTrader.js

--- algotrading-client-react/src/AlgoTrader.js
+++ algotrading-client-react/src/AlgoTrader.js
@@ -5,16 +5,63 @@ import Button from "./components/common/button";
 import {useEffect, useState} from "react";
 import io from "socket.io-client";
 import Badge from "./components/common/badge";
+import {Line} from "react-chartjs-2";
+import {CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, Tooltip, PointElement, Title} from "chart.js";
+import Table from "./components/common/table";
 
 const socket = io("ws://127.0.0.1:5555");
+const options = {
+    responsive: false,
+    animation: false,
+    maintainAspectRatio: true,
+    scales: {
+        y: {
+            type: 'linear',
+            position: 'left',
+            stack: 'demo',
+            stackWeight: 2
+        }
+    },
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'BINANCE Market Data',
+        }
+    }
+};
+ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend);
 
 function AlgoTrader() {
     const [symbol, setSymbol] = useState("BTCUSDT");
     const [symbols, setSymbols] = useState([]);
     const [volume, setVolume] = useState(0);
     const [totalVolume, setTotalVolume] = useState(0);
+    const [windowSize, setWindowSize] = useState(50);
+    const [trades,setTrades] = useState([]);
     const [connected, setConnected] = useState(false);
-
+    const [chartData, setChartData] = useState({
+        labels: [],
+        datasets: [{
+            label: 'BTC-USDT Price',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderDashOffset: 0.0,
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: []
+        }]
+    });
     useEffect(() => {
         const controller = new AbortController();
 
@@ -54,8 +101,20 @@ function AlgoTrader() {
     useEffect(() => {
         socket.on("ticker", (trade) => {
             setVolume(trade.volume);
+            setTrades([...trades, trade]);
+            const newChartData = {...chartData};
+            newChartData.labels = [...chartData.labels, trade.timestamp];
+            newChartData.datasets = [...chartData.datasets];
+            newChartData.datasets[0].data = [...chartData.datasets[0].data,Number(trade.price)];
+            if (newChartData.datasets[0].data.length > windowSize){
+                newChartData.datasets[0].data.slice(newChartData.datasets[0].data.length-windowSize);
+            }
+            if (newChartData.labels.length > windowSize){
+                newChartData.labels.slice(newChartData.labels.length-windowSize);
+            }
+            setChartData(newChartData);
         });
-    }, []);
+    }, [trades,windowSize,chartData]);
     return (
         <Container>
             <p></p>
@@ -65,6 +124,12 @@ function AlgoTrader() {
                         label={"Symbol"}
                         value={symbol}
                         optionValues={symbols}
+                />
+                <SelectBox handleChange={(e) => setWindowSize(e.target.value)}
+                           id={"windowSize"}
+                           label={"Window Size"}
+                           value={windowSize}
+                           optionValues={[25,50,100,250]}
                 />
                 {
                     connected &&
@@ -80,6 +145,13 @@ function AlgoTrader() {
                 }
             </Card>
             <p></p>
+            <Card title={"Market Data"}>
+                <Table values={trades}
+                       keyField={"timestamp"}
+                       handleRowClick={()=>{}}
+                       headers={["Price","Quantity","Timestamp"]}
+                       fields={["price","quantity","timestamp"]} />
+            </Card>
             <Card title={"Market Trend"}>
                 <Badge label={"Market Volume"}
                        color={"bg-primary"}
@@ -88,6 +160,10 @@ function AlgoTrader() {
                 <Button click={()=>{setTotalVolume(0)}}
                         color={"btn-danger"}
                         label={"Reset Total Volume"}/>
+                <Line data={chartData}
+                      width={1080}
+                      height={720}
+                      options={options}/>
             </Card>
         </Container>
     );
